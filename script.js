b60c39c9f94511380aa833f69ba2ae32429d0828
dvk2807 <dovhyivolodymyr28@gmail.com>
2025-08-31 09:34:26 +0200

    Implement letter input

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ Game.wordLength = 5;
 Game.guessCount = 6;
 Game.letters = "АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ'"
 
+Game.guess = "";
+Game.guesses = [];
+
 Game.setUpBoard = function(){
     Game.board.style.gridTemplateRows = `repeat(${Game.guessCount}, 1fr)`;
     Game.board.style.gridTemplateColumns = `repeat(${Game.wordLength}, 1fr)`;
@@ -20,15 +23,29 @@ Game.setUpBoard = function(){
     }
 };
 
-Game.setUpKeyBoard = function(){
-    characters = Game.letters + "❌✅"
-    for(var i = 0; i < characters.length; i++){
+Game.setUpKeyboard = function(){
+    for(var i = 0; i < Game.letters.length; i++){
         key = document.createElement("div");
         key.id = `key-${i}`;
         key.classList.add("key");
-        key.innerHTML = characters[i];
+        key.innerHTML = Game.letters[i];
+        key.setAttribute("onclick", `Game.input(${i})`);
         Game.keyboard.appendChild(key);
     }
+
+    erase = document.createElement("div");
+    erase.id = "key-erase";
+    erase.classList.add("key");
+    erase.innerHTML = "❌";
+    erase.setAttribute("onclick", `Game.input(34)`);
+    Game.keyboard.appendChild(erase);
+
+    enter = document.createElement("div");
+    enter.id = "key-enter";
+    enter.classList.add("key");
+    enter.innerHTML = "✅";
+    enter.setAttribute("onclick", `Game.input(35)`);
+    Game.keyboard.appendChild(enter);
 };
 
 Game.togglePanel = function(side){
@@ -36,5 +53,34 @@ Game.togglePanel = function(side){
     panelContent.classList.toggle("panel-enabled");
 };
 
+Game.input = function(index){
+    x = Game.guess.length;
+    y = Game.guesses.length;
+    if(index == 34){
+        // Erase
+        if(Game.guesses.length >= Game.guessCount) return;
+        if(Game.guess.length <= 0) return;
+        Game.guess = Game.guess.slice(0, -1);
+        Game.setTile(x - 1, y, -1)
+    }else if(index == 35){
+        if(Game.guess.length != Game.wordLength) return;
+        // Enter
+        Game.guesses.push(Game.guess)
+        Game.guess = ""
+    }else{
+        // Type a letter
+        if(Game.guess.length >= Game.wordLength) return;
+        if(Game.guesses.length >= Game.guessCount) return;
+        Game.guess += Game.letters[index];
+        Game.setTile(x, y, index);
+    }
+};
+
+Game.setTile = function(x, y, index){
+    tile = document.getElementById(`tile-${y}-${x}`);
+    if(index != -1) tile.innerHTML = Game.letters[index];
+    else tile.innerHTML = "";
+};
+
 Game.setUpBoard();
-Game.setUpKeyBoard();
+Game.setUpKeyboard();
